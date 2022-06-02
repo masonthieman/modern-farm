6d98f0537133e8b1b64b9fdc5288715199f58846
Mason Thieman <masonthieman@gmail.com>
2022-06-02 13:18:35 -0500

    Add styling

--- src/scripts/catalog.js
+++ src/scripts/catalog.js
@@ -1,5 +1,16 @@
+
+// Creates an html string representing the catalog of harvested plants
 export const Catalog = harvestedArray => {
+
+    // Instantiate empty html string
+    let htmlString = ""
+
+    // Add a section component for each plant in the harvested array
     for (const plant of harvestedArray) {
-        
+        htmlString += `<section class="plant">${plant.type}</section>`
     }
+
+    document.getElementById("catalog").innerHTML = htmlString
+    // Return the catalog HTML string
+    return htmlString
 }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,19 @@
+// Import module functions
 import { createPlan } from './plan.js'
-import { createAsparagus } from './seeds/asparagus.js'
-import { createCorn } from './seeds/corn.js'
-import { addPlant, usePlants } from './field.js'
+import { usePlants } from './field.js'
 import { plantSeeds } from './tractor.js'
 import { harvestPlants } from './harvester.js'
+import { Catalog } from './catalog.js'
 
+// Create the yearly plan 2D array with the provided function
 const yearlyPlan = createPlan()
 
+// Create the planted seeds array based on the plan array
 const plantedSeeds = usePlants(plantSeeds(yearlyPlan))
 
-console.log(plantedSeeds)
-console.log(harvestPlants(plantedSeeds))
+// Harvest planted seeds and create a harvested plant array
+const harvestedPlants = harvestPlants(plantedSeeds)
+
+// Display a catalog of the harvested plants
+Catalog(harvestedPlants)
+
